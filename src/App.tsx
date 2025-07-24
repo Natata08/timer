cef23d91761976f8e56decd25a7475d6184f74f0
Natalia Lapina <lapinann08@gmail.com>
2025-07-24 16:09:11 +0200

    feat: add generic Container component with polymorphic 'as' prop support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Input from './components/Input.tsx'
+import Container from './components/Container.tsx'
+import Button from './components/Button.tsx'
 
 function App() {
   return (
     <main>
       <Input id='name' label='Name' type='text'></Input>
       <Input id='age' label='Age' type='number'></Input>
+      <Container as={Button} type='button'>
+        Click me
+      </Container>
     </main>
   )
 }
