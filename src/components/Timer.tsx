ed184b5c1c135d746eefed5a4c557ab65de02b14
Natalia Lapina <lapinann08@gmail.com>
2025-07-25 16:35:48 +0200

    fix: prevent memory leaks by clearing timer intervals and adding isRunning dependency

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,20 +1,38 @@
 import Container from './UI/Container.tsx'
-import type { Timer as TimerProps } from '../store/timers-context.tsx'
-import { useEffect, useState } from 'react'
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from '../store/timers-context.tsx'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Timer({ name, duration }: TimerProps) {
+  const intervalRef = useRef<number>(null)
   const [remainingTime, setRemainingTime] = useState(duration * 1000)
+  const { isRunning } = useTimersContext()
+
+  if (remainingTime <= 0 && intervalRef.current) {
+    clearInterval(intervalRef.current)
+  }
 
   useEffect(() => {
-    setInterval(() => {
-      setRemainingTime((prevTime) => {
-        if (prevTime <= 0) {
-          return prevTime
-        }
-        return prevTime - 50
-      })
-    }, 50)
-  }, [])
+    let timer: number
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime
+          }
+          return prevTime - 50
+        })
+      }, 50)
+
+      intervalRef.current = timer
+    } else if (intervalRef.current) {
+      clearInterval(intervalRef.current)
+    }
+
+    return () => clearInterval(timer)
+  }, [isRunning])
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
 
