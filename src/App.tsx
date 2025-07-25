fc025c09c1f96a92c3b361b96befc64a77f9601f
Natalia Lapina <lapinann08@gmail.com>
2025-07-25 14:00:46 +0200

    feat: implement basic timer components structure and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import Button from './components/Button.tsx'
-import Form, { type FormHandle } from './components/Form.tsx'
-import Input from './components/Input.tsx'
-import { useEffect, useRef } from 'react'
+import AddTimer from './components/AddTimer.tsx'
+import Header from './components/Header.tsx'
+import Timers from './components/Timers.tsx'
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const customFormRef = useRef<FormHandle>(null)
-
-  useEffect(() => {
-    inputRef.current?.focus()
-  }, [])
-
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string }
-    console.log(extractedData)
-    customFormRef.current?.clear()
-  }
-
   return (
-    <main>
-      <Form onSave={handleSave} ref={customFormRef}>
-        <Input id='name' label='Name' type='text' ref={inputRef}></Input>
-        <Input id='age' label='Age' type='number'></Input>
-        <Button>Save</Button>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   )
 }
 
