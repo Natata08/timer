9890287d99edb282bcc52eb35f7adf829c382546
Natalia Lapina <lapinann08@gmail.com>
2025-07-25 14:42:51 +0200

    feat: create initial timer components with basic structure and UI elements

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Button from './UI/Button.tsx'
+import { useTimersContext } from '../store/timers-context.tsx'
+
+export default function Header() {
+  const timersCtx = useTimersContext()
+  return (
+    <header>
+      <h1>Timer</h1>
+
+      <Button>{timersCtx.isRunning ? 'Stop' : 'Start'} Timers</Button>
+    </header>
+  )
+}
